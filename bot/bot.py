fba9394f48123b63d9d8356280e7e0a6e1947deb
Dekriel <92822789+Dekriel@users.noreply.github.com>
2021-12-30 18:41:11 +0000

    im coming for your house

--- bot/bot.py
+++ bot/bot.py
@@ -3,7 +3,7 @@ from disnake.ext.commands import Bot
 
 bot = Bot(command_prefix=['hey jarvide, ', 'hey ', '!', 'jarvide '], help_command=None)
 
-@bot.listen()
+@bot.event
 async def on_ready():
     print('Jarvide is ready.')
 
