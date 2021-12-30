dd6e75111035cbb051118ff2ab7beab1c1ebf11b
boopdev <appleifry@gmail.com>
2021-12-30 11:53:28 -0400

    Make Jarvide subclass instead of having an instance of Bot

--- bot/bot.py
+++ bot/bot.py
@@ -1,25 +1,39 @@
 import disnake
-from disnake.ext.commands import Bot
+from disnake.ext.commands import Bot, Context, command
 
-bot = Bot(command_prefix=['hey jarvis, ', 'hey ', '!'], help_command=None)
+class Jarvide(Bot):
+    def __init__(self, *args, **kwargs) -> None:
 
-@bot.command()
-async def on_ready():
-    print('Jarvide is ready.')
+        # Initialize the parent class
+        super().__init__(
+            command_prefix=['hey jarvis, ', 'hey ', '!'],
+            help_command=None
+        )
 
+        # Setup the basic commands
+        self.add_listener(self.readyListener, "on_ready")
+        self.add_command(self._ping)
 
-@bot.command()
-async def ping(ctx):
-    if round(bot.latency * 1000) > 150:
-        health = "unhealthy"
-    else:
-        health = "healthy"
-    ping_embed = disnake.Embed(
-        title="Pong!",
-        description=f"Jarvide ping: **{round(bot.latency * 1000)}ms** \n**health**: \n {health}",
-        color=disnake.Color.yellow()
+    async def readyListener(self, *args, **kwargs) -> None:
+        """Logs when the bot is ready"""
+        print(
+            "Jarvide is ready."
+        )
+
+    @command(name="ping")
+    async def _ping(ctx):
+        jarvisLatency = round(bot.latency * 1000)
+
+        return await ctx.send(
+            embed = disnake.Embed(
+                title = "Pong!",
+                description = f"Jarvis ping: **{jarvisLatency}ms**\n**Health:** {'healthy' if jarvisLatency < 150 else 'unhealthy'}",
+                color = disnake.Color.yellow()
+            )
+        )
+
+if __name__ == "__main__":
+    bot = Jarvide()
+    bot.run(
+        "OTI0MjE3OTY4MTE4ODg2NDEw.YcbXDg.kKYc71LnpLhdDC2oDS94qHrroNs"
     )
-    await ctx.send(embed=ping_embed)
-
-
-bot.run(TOKEN)
