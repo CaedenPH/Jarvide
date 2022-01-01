0f8d3d7516d378903b6f026ffdfcf5128fac09e2
Okimii <92546867+Okimii@users.noreply.github.com>
2021-12-31 20:53:35 -0800

    made fun.py cleaner
    fixed spacing errors

--- src/cogs/fun.py
+++ src/cogs/fun.py
@@ -4,33 +4,34 @@ import random
 
 
 class Fun(commands.Cog):
-    def __init__(self,bot: commands.Bot) -> None:
+    """Fun cog for fun related commands"""
+    def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
 
-    @commands.command(aliases=["gaypercent","howgay"])
+    @commands.command(aliases=["gaypercent", "howgay"])
     async def gaymeter(self, ctx, member: disnake.Member=None) -> None:
-        if member is None:
+        if not member:
             member = ctx.author
 
         await ctx.send(f"{member.mention} is {random.randint(1,100)}% gay")
 
 
-    @commands.command(aliases=["Y_or_N","choose"])
+    @commands.command(aliases=["Y_or_N", "choose"])
     async def pick(self, ctx) -> None:
-        await ctx.send(random.choice(["Yes","No"]))
+        await ctx.send(random.choice(["Yes", "No"]))
 
 
-    @commands.command(aliases=["cutepercent","howcute"])
-    async def cutemeter(self, ctx,member: disnake.Member=None) -> None:
-        if member is None:
+    @commands.command(aliases=["cutepercent", "howcute"])
+    async def cutemeter(self, ctx, member: disnake.Member=None) -> None:
+        if not member:
             member = ctx.author
 
         await ctx.send(f"{member.mention} is {random.randint(1,100)}% cute")
 
 
     @commands.command(aliases=["should_i"])
-    async def options(self, ctx,*, options=None) -> None:
+    async def options(self, ctx, *, options=None) -> None:
         if options is None:
             return await ctx.send("no options were given")
 
@@ -39,17 +40,17 @@ class Fun(commands.Cog):
 
     @commands.command(aliases=["love"])
     async def kiss(self, ctx, member: disnake.Member=None) -> None:
-        if member is None:
+        if not member:
             return await ctx.send(f"{ctx.author.mention} has kissed himself")
         embed1 = disnake.Embed(
-            title=f"how cute,{ctx.author.mention} has kissed {member.name}").set_image(url="https://media.tenor.co/videos/fc567d93fe70d2e0567325df0410959b/mp4")
+            title=f"how cute, {ctx.author.mention} has kissed {member.name}").set_image(url="https://media.tenor.co/videos/fc567d93fe70d2e0567325df0410959b/mp4")
 
         await ctx.send(embed=embed1)
     
 
-    @commands.command(aliases=["smack","hit"])
+    @commands.command(aliases=["smack", "hit"])
     async def slap(self, ctx, member: disnake.Member=None) -> None:
-        if member is None:
+        if not member:
             return await ctx.send(f"{ctx.author.mention} has slapped himself")
 
         embed2 = disnake.Embed(
@@ -59,16 +60,16 @@ class Fun(commands.Cog):
 
 
     @commands.command(aliases=["simppercent"])
-    async def simpmeter(self, ctx,member: disnake.Member=None) -> None:
-        if member is None:
+    async def simpmeter(self, ctx, member: disnake.Member=None) -> None:
+        if not member:
             member = ctx.author
 
         await ctx.send(f"{member.mention} is {random.randint(1,100)}% a simp")
 
 
     @commands.command(aliases=["howbig"])
-    async def ppmeter(self, ctx,member: disnake.Member=None) -> None:
-        if member is None:
+    async def ppmeter(self, ctx, member: disnake.Member=None) -> None:
+        if not member:
             member = ctx.author
         size = random.randint(1,12)
         if size <= 6:
@@ -78,4 +79,5 @@ class Fun(commands.Cog):
 
 
 def setup(bot: commands.Bot) -> None:
+    """Setup fun cog"""
     bot.add_cog(Fun(bot))
