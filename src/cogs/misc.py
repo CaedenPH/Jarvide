ce41c9ef2949a968155dbae2bbfed186b08b7dfe
osam7a <90650351+osam7a@users.noreply.github.com>
2022-01-03 18:56:29 +0400

    More commands to misc.py
    Still needs to test but here

--- src/cogs/misc.py
+++ src/cogs/misc.py
@@ -1,4 +1,4 @@
-import disnake
+import disnake, aiohttp, random
 
 from disnake.ext import commands
 
@@ -11,6 +11,13 @@ class Misc(commands.Cog):
 
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+    
+    async def overlay(ctx: commands.Context, endpoint, user = None):
+                if user == None: user = ctx.author
+                emb = disnake.Embed(
+                    color = 0x90EE90
+                ).set_image(url = f"https://some-random-api.ml/canvas/%s?avatar=%s" % (endpoint, user.avatar.with_format('png').url))
+                await ctx.send(embed = emb)
 
     @commands.command(aliases=["latency"])
     async def ping(self, ctx: commands.Context):
@@ -33,6 +40,86 @@ class Misc(commands.Cog):
         )
         await ctx.send(content="🏓**Pong**", embed=embed)
 
+    @commands.command(aliases = ['insult'])
+    async def roast(self, ctx: commands.Context, *, user: disnake.Member = None):
+      async with aiohttp.ClientSession() as cs:
+          async with cs.get("https://evilinsult.com/generate_insult.php?lang=en&type=json") as res:
+            resp = await res.json()
+            insult = resp['insult']
+            color = 0x90EE90
+            embed = (
+            disnake.Embed(color=color, description = insult)
+        )
+            await ctx.send(f"{user.mention}", embed=embed)
+    
+    @commands.command()
+    async def meme(self, ctx: commands.Context):
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(f"https://reddit.com/r/dankmemes.json?sort=hot") as resp:
+                _json = await resp.json()
+                color = 0x90EE90
+                post = random.choice(_json['data']['children'])['data']
+                embed = (
+                    disnake.Embed(
+                        color = color,
+                        title = post['title'],
+                        url = post['permalink']
+                    ).set_image(url =post['url'])
+                )
+                await ctx.send(embed = embed)
+
+    @commands.command()
+    async def reddit(self, ctx: commands.Context, subreddit):
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(f"https://reddit.com/r/{subreddit}.json?sort=hot") as resp:
+                _json = await resp.json()
+                post = random.choice(_json['data']['children'])['data']
+                if 'url' in post:
+                    embed = (
+                        disnake.Embed(color= 0x90EE90, title = post['title'], url = post['permalink'])
+                    ).set_image(url = post['url'])
+                else:
+                    embed = (
+                        disnake.Embed(color= 0x90EE90, title = post['title'], url = post['permalink'], description = post['selftext'])
+                    )
+
+                await ctx.send(embed=embed)    
+
+    @commands.command()
+    async def gay(self, ctx, *, user: disnake.Member = None):
+        if user == None:
+            await self.overlay(ctx, "gay")
+        else: await self.overlay(ctx, "gay", user)    
+    
+    @commands.command()
+    async def wasted(self, ctx, *, user: disnake.Member = None):
+        if user == None:
+            await self.overlay(ctx, "wasted")
+        else: await self.overlay(ctx, "wasted", user) 
+
+    @commands.command()
+    async def passed(self, ctx, *, user: disnake.Member = None):
+        if user == None:
+            await self.overlay(ctx, "passed")
+        else: await self.overlay(ctx, "passed", user)     
+
+    @commands.command()
+    async def jail(self, ctx, *, user: disnake.Member = None):
+        if user == None:
+            await self.overlay(ctx, "jail")
+        else: await self.overlay(ctx, "jail", user)    
+
+    @commands.command()
+    async def comrade(self, ctx, *, user: disnake.Member = None):
+        if user == None:
+            await self.overlay(ctx, "comrade")
+        else: await self.overlay(ctx, "comrade", user)      
+    
+    @commands.command()
+    async def triggered(self, ctx, *, user: disnake.Member = None):
+        if user == None:
+            await self.overlay(ctx, "triggered")
+        else: await self.overlay(ctx, "triggered", user)    
 
 def setup(bot: commands.Bot) -> None:
     """Setup misc cog"""
