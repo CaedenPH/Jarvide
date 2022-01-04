c84e256e542029deaa48d326f22b31e03eb53957
CaedenPH <caedenperelliharris@gmail.com>
2022-01-04 17:00:45 +0000

    revert bot

--- src/bot.py
+++ src/bot.py
@@ -1,27 +1,57 @@
+import datetime
 import os
 import string
 import copy
-import disnake
+import typing
 
-from disnake.ext import commands
-import aiosqlite
+from disnake import Message
+
+from disnake.ext.commands import (
+    Bot,
+    BotMissingPermissions,
+    MissingPermissions,
+    MissingRole,
+    DisabledCommand,
+    NotOwner,
+    ChannelNotFound,
+    MemberNotFound,
+    UserNotFound,
+    TooManyArguments,
+    CommandOnCooldown,
+    MissingRequiredArgument,
+    Context
+)
 from motor.motor_asyncio import AsyncIOMotorClient
 from odmantic import AIOEngine
 
 from .HIDDEN import TOKEN, MONGO_URI
 
+REMOVE_WORDS = [
+    "what",
+    "pls",
+    "tell",
+    "me",
+    "the",
+    "tf",
+    "give",
+    "your",
+    "you",
+    "is",
+    "can",
+]
 
-class Jarvide(commands.Bot):
+
+class Jarvide(Bot):
     def __init__(self):
         super().__init__(
             command_prefix="jarvide",
             case_insensitive=True,
             strip_after_prefix=True,
             help_command=None,  # type: ignore
-            intents=disnake.Intents.all(),
         )
         self.engine = AIOEngine(AsyncIOMotorClient(MONGO_URI))
         self.send_guild = None
+        self.error_channel = None
 
     def setup(self) -> None:
         for filename in os.listdir("./src/cogs"):
@@ -35,18 +65,28 @@ class Jarvide(commands.Bot):
         self.setup()
         super().run(TOKEN, reconnect=True)
 
-    async def on_message(self, original_message: disnake.Message) -> None:
-        # Prevent bots from executing commands
+    async def on_message(self, original_message: Message) -> typing.Optional[Message]:
+        if self.user.mentioned_in(original_message):
+            return await original_message.channel.send("My prefix is jarvide")
         if (
             original_message.author.bot
             or "jarvide" not in original_message.content.lower()
         ):
             return
 
+        original_message.content = " ".join(
+            [
+                word
+                for word in original_message.content.lower().split()
+                if not any(
+                    word.startswith(censored_word) for censored_word in REMOVE_WORDS
+                )
+            ]
+        )
         message_content = "".join(
             [
                 char
-                for char in original_message.content.lower()
+                for char in original_message.content
                 if char not in string.punctuation
             ]
         )
@@ -56,16 +96,18 @@ class Jarvide(commands.Bot):
         )
 
         list_of_commands = {c: [c.name, *c.aliases] for c in self.commands}
-
         commands_in_message = list(
             filter(
                 lambda c: any([x in message_content.split() for x in c[1]]),
                 list_of_commands.items(),
             )
         )
+        if len(commands_in_message) <= 0:
+            return
 
-        if len(commands_in_message) != 1:
-            return  # TODO: Maybe make the user know that they supplied too many commands?
+        if "help" in [k[0].name for k in commands_in_message]:
+            if commands_in_message[0][0].name != "help":
+                commands_in_message = commands_in_message[::-1]
 
         cmd = commands_in_message[0][0]
         ctx = await super().get_context(original_message)
@@ -81,41 +123,55 @@ class Jarvide(commands.Bot):
             )[2]
 
             new_message = copy.copy(original_message)
-            new_message.content = f"jarvide {cmd.name} {args}"
+            new_message.content = f"jarvide {cmd.name}{args}"
             await super().process_commands(new_message)
 
     async def on_ready(self) -> None:
         self.send_guild = self.get_guild(926811692019626064)
+        self.error_channel = self.get_channel(927596019468873748)
         print("Set up")
-    
-    async def on_guild_join(guild)
-        await guild.system_channel.send(embed = disnake.Embed(title = f"Thank you for adding my bot!", description = f"""
-        Hello, i am jarvide Bot And IDE ```yaml\nHow to use Ide command:
 
-        To start: run the command; [jarvide ide]
+    def underline(self, text, at, for_):
+        underline = (" " * at) + ("^" * for_)
+        return text + "\n" + underline
 
-Note:
-    - all phases have an exit button; to open a new IDE, you have to exit your open ide. (one ide open per channel)
+    async def on_command_error(self, ctx: Context, error: Exception):
+        if isinstance(error, MissingRequiredArgument):
+            desc = f"{ctx.prefix} {ctx.command.name} {ctx.command.signature}"
+            inside = self.underline(desc, desc.index(f'<{error.param.name}>'), len(f'<{error.param.name}>'))
+            desc = f"You missed an argument: \n```\n{inside}\n```"
+            return await ctx.send(desc)
 
-Creating a file (OpenFile phase):
-    - press the create button 
-       ➥ enter a filename; 
-            [must be shorter than 12 characters long.]
-            [if you want to run the file, you must put the file extension. eg if you want it to be ran in python, you put .py in the filename]
-       ➥ enter the content;
-            [you can use discord codeblocks if you want]
-            [this can be edited and changed]
+        elif isinstance(error, DisabledCommand):
+            return await ctx.send('This command is disabled.')
 
-    + you have just created a file. You have now moved onto the FileView phase!
-    + you can also open files via    links, uploading, github and saved files 
-        ➥ [ supported links are (toptal, pastebin, ghostbin)]'n```"""))
-        async with aiosqlite.connect("databases/config.db") as db:
-            async with db.cursor() as cur:
-                guilds = (i[0] for i in await cur.execute("SELECT * FROM guilds"))
-                if guild.id not in guilds:
-                    await cur.execute("INSERT INTO guilds (id, allowedChannels) VALUES (%s, %s)" % (guild.id, (ctx.guild.system_channel)
-                else: return
-                await cur.commit()  
+        elif isinstance(error, TooManyArguments):
+            return await ctx.send(f'Too many arguments passed.\n```yaml\nusage: {ctx.prefix}{ctx.command.aliases.append(ctx.command.name)} {ctx.command.signature}')
 
-    async def on_command_error(self, ctx, error):
-        await ctx.send(error)
+        elif isinstance(error, CommandOnCooldown):
+            return await ctx.send(f'Command is on cooldown. Try again after {datetime.timedelta(seconds = error.retry_after)}')
+
+        elif isinstance(error, NotOwner):
+            return await ctx.send('Only my owner can use this command.')
+
+        elif isinstance(error, MemberNotFound):
+            return await ctx.send('No such member found.')
+
+        elif isinstance(error, UserNotFound):
+            return await ctx.send('No such user found.')   
+
+        elif isinstance(error, ChannelNotFound):
+            return await ctx.send('No such channel found.')
+
+        elif isinstance(error, MissingPermissions):
+            return await ctx.send(f'You need the {"".join(error.missing_permissions)} permissions to be able to do this.')
+
+        elif isinstance(error, BotMissingPermissions):
+            return await ctx.send(f'I need the {"".join(error.missing_permissions)} permissions to be able to do this.')
+
+        elif isinstance(error, MissingRole):
+            return await ctx.send('You are missing a certain role to perform this command.')
+        else:
+            await ctx.send("An unexpected error occured! Reporting this to my developer...")
+            await self.error_channel.send(error)
+            raise error
