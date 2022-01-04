ee02ee91431f388d1b2d37769be669514c20eb3c
osam7a <90650351+osam7a@users.noreply.github.com>
2022-01-04 13:50:04 +0400

    better error handler (cleaner)
    i made a better error handler (shows the missing permission and underlines it, shows how much left for a cooldown etc)

--- src/bot.py
+++ src/bot.py
@@ -124,22 +124,43 @@ class Jarvide(Bot):
     async def on_ready(self) -> None:
         self.send_guild = self.get_guild(926811692019626064)
         print("Set up")
-
+    
+    def underline(text, at, for_):
+            import itertools
+            underline = "".join(itertools.repeat(" ", at)) + "".join(itertools.repeat("^", for_))
+            return text + "\n" + underline
+    
     async def on_command_error(self, ctx: Context, error: Exception):
+        
         if isinstance(error, MissingRequiredArgument):
-            return await ctx.send(f'```py\n{ctx.command.name} {ctx.command.signature}\n```\nNot enough arguments passed.')
+            aliases = ctx.command.aliases.append(ctx.command.name)
+            desc = f"```\n{ctx.prefix}[{'/'.join(aliases)}] {ctx.command.signature}\n```"
+            desc = underline(desc, desc.index(f"<{errororiginal.param}>", len(f"<{errororiginal.param}")))
+            return await ctx.send(desc)
 
         elif isinstance(error, CommandNotFound):
+            results = []
+            cmd = ctx.message.split(ctx.prefix)[0]
+            for i in ctx.bot.commands:
+                
+                if len(difflib.get_close_matches(
+                                            cmd, i.name
+                                            )) > 0:
+                    results.append(
+                        f"- `{i.name}`\n"
+                    )
+                    
+            await ctx.send(f"No such a command, you meant any of those?\n{results}")        
             return
 
         elif isinstance(error, DisabledCommand):
             return await ctx.send('This command is disabled.')
 
         elif isinstance(error, TooManyArguments):
-            return await ctx.send('Too many arguments passed.')
+            return await ctx.send(f'Too many arguments passed.\n```yaml\nusage: {ctx.prefix}{ctx.command.aliases.append(ctx.command.name)} {ctx.command.signature}')
 
         elif isinstance(error, CommandOnCooldown):
-            return await ctx.send('Command is on cooldown. Try again later.')
+            return await ctx.send(f'Command is on cooldown {error.rate} uses per {datetime.timedelta(seconds = error.per)} Try again after {datetime.timedelta(seconds = error.retry_after)}}')
 
         elif isinstance(error, NotOwner):
             return await ctx.send('Only my owner can use this command.')
@@ -154,10 +175,10 @@ class Jarvide(Bot):
             return await ctx.send('No such channel found.')
 
         elif isinstance(error, MissingPermissions):
-            return await ctx.send('You cannot invoke this command because you do not have enough permissions.')
+            return await ctx.send(f'You need the {"".join(error.missing_permissions)} permissions to be able to do this')
 
         elif isinstance(error, BotMissingPermissions):
-            return await ctx.send('I cannot execute this command because I am missing certain permissions.')
+            return await ctx.send(f'I need the {"",join(error.missinng_permissions)} permissions to be able to do this')
 
         elif isinstance(error, MissingRole):
             return await ctx.send('You are missing a certain role to perform this command.')
