d385cf8ce32527c73f76fba45f4dc812b6778044
NotFlameDev <dragogamez47@gmail.com>
2022-01-10 07:09:49 +0700

    Put return

--- src/cogs/ide/auto_detect.py
+++ src/cogs/ide/auto_detect.py
@@ -4,7 +4,6 @@ import disnake
 import re
 import asyncio
 import simpleeval
-import string
 
 from disnake.ext import commands
 from typing import Optional
@@ -207,7 +206,7 @@ class Listeners(commands.Cog):
                 value="```yaml\nImagine you have zero cookies and you split them amongst 0 friends, how many cookies does each friend get? See, it doesn't make sense and Cookie Monster is sad that there are no cookies, and you are sad that you have no friends.```"
             )
         except simpleeval.FeatureNotAvailable:
-            await message.channel.send("That syntax is not available currently, sorry!")
+            return await message.channel.send("That syntax is not available currently, sorry!")
         try:
             await message.channel.send(embed=embed)
         except disnake.HTTPException:
